5d65247e8adadc2ed244c73795ad8cc6eb243d94
fatematuzjohora <fatematuzjohora889@gmail.com>
2022-05-26 10:56:49 +0600

    heroku added

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 
@@ -31,7 +30,6 @@ app.use(express.json());
 // }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b0ojei0.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -50,11 +48,12 @@ const run = async () => {
       res.send(products);
     });
     await client.connect();
-    const reviewCollection = client.db("HopLight").collection("reviews");
-    const userCollection = client.db("HopLight").collection("users");
-    const profileCollection = client.db("HopLight").collection("profile");
-    const purchaseCollection = client.db("HopLight").collection("purchase");
-    const paymentCollection = client.db("HopLight").collection("payments");
+    const reviewCollection = client.db("make-choices").collection("reviews");
+    const userCollection = client.db("make-choices").collection("users");
+    const profileCollection = client.db("make-choices").collection("profile");
+    const purchaseCollection = client.db("make-choices").collection("purchase");
+    const paymentCollection = client.db("make-choices").collection("payments");
+    const orderCollenction = client.db("make-choices").collection("orders");
 
     const verifyAdmin = async (req, res, next) => {
       const requester = req.decoded.email;
@@ -67,7 +66,7 @@ const run = async () => {
         res.status(403).send({ message: "forbidden" });
       }
     };
-   
+
     app.post("/create-payment-intent", async (req, res) => {
       const service = req.body;
       const price = service.price;
@@ -87,7 +86,7 @@ const run = async () => {
     });
     app.post("/review", async (req, res) => {
       const review = req.body;
-    
+
       const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
@@ -102,146 +101,206 @@ const run = async () => {
       const product = await productsCollection.findOne(query);
       res.send(product);
     });
-    
-app.delete("/product/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: ObjectId(id) };
-  const result = await productsCollection.deleteOne(query);
-  res.send(result);
-});
-app.get("/user", async (req, res) => {
-  const users = await userCollection.find().toArray();
-  res.send(users);
-});
-app.put("/profile/:email", async (req, res) => {
-  const email = req.params.id;
-  const users = req.body;
-  const filter = { email: email };
-  const options = { upsert: true };
-  const updateDoc = {
-    $set: {
-      name: users.name,
-      education: users.education,
-      job: users.job,
-      phone: users.phone,
-      city: users.city,
-    },
-  };
-  const result = profileCollection.updateOne(filter, updateDoc, options);
-  res.send(result);
-});
-app.get("/profile/:email", async (req, res) => {
-  const email = req.params.id;
-  const profile = req.body;
-  const filter = { email: email };
-  const result = await profileCollection.findOne(filter);
-  res.send(result);
-});
-app.get("/profile/:user.email", async (req, res) => {
-  const user = req.params.user;
-  const query = { email: user };
-  const profile = await profileCollection.findOne(query);
-  res.send(profile);
-});
-app.get("/admin/:email", async (req, res) => {
-  const email = req.params.email;
-  const user = await userCollection.findOne({ email: email });
-  const isAdmin = user?.role === "admin";
-  res.send({ admin: isAdmin });
-});
-app.delete("/user/admin/:email", async (req, res) => {
-  const email = req.params.email;
-  const user = await userCollection.findOne({ email: email });
-  const isAdmin = user?.role === "admin";
-  if (isAdmin) {
-    const result = await userCollection.deleteOne({ email: email });
-    res.send(result);
-  } else {
-    res.send({ message: "Forbidden" });
-  }
-});
-app.put("/user/admin/:email", async (req, res) => {
-  const email = req.params.email;
-  const filter = { email: email };
-  const updateDoc = {
-    $set: { role: "admin" },
-  };
-  const result = await userCollection.updateOne(filter, updateDoc);
-  res.send(result);
-});
-app.put("/user/:email", async (req, res) => {
-  const email = req.params.email;
-  const user = req.body;
-  const filter = { email: email };
-  const options = { upsert: true };
-  const updateDoc = {
-    $set: user,
-  };
 
-  const result = await userCollection.updateOne(filter, updateDoc, options);
-  const token = jwt.sign(
-    { email: email },
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "12h" }
-  );
-  res.send({ result, token });
-});
-app.put("/purchase/:id", (req, res) => {
-  const id = req.params.id;
-  const total = req.body;
-  const filter = { _id: ObjectId(id) };
-  const options = { upsert: true };
-  const updateDoc = {
-    $set: {
-      name: total.name,
-      productName: total.productName,
-      phone: total.phone,
-      quantity: total.quantity,
-      price: total.price,
-      email: total.email,
-    },
-  };
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.get("/user", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+    app.put("/profile/:email", async (req, res) => {
+      const email = req.params.id;
+      const users = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: users.name,
+          education: users.education,
+          job: users.job,
+          phone: users.phone,
+          city: users.city,
+        },
+      };
+      const result = profileCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    app.get("/profile/:email", async (req, res) => {
+      const email = req.params.id;
+      const profile = req.body;
+      const filter = { email: email };
+      const result = await profileCollection.findOne(filter);
+      res.send(result);
+    });
+    app.get("/profile/:user.email", async (req, res) => {
+      const user = req.params.user;
+      const query = { email: user };
+      const profile = await profileCollection.findOne(query);
+      res.send(profile);
+    });
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+    app.delete("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      if (isAdmin) {
+        const result = await userCollection.deleteOne({ email: email });
+        res.send(result);
+      } else {
+        res.send({ message: "Forbidden" });
+      }
+    });
+    app.put("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
 
-  const result = purchaseCollection.updateOne(filter, updateDoc, options);
-  res.send(result);
-});
-app.patch("/purchase/:id", async (req, res) => {
-  const id = req.params.id;
-  const payment = req.body;
-  const filter = { _id: ObjectId(id) };
-  const updatedDoc = {
-    $set: {
-      paid: true,
-      transactionId: payment.transactionId,
-    },
-  };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      // const token = sign(
+      //   { email: email },
+      //   // process.env.ACCESS_TOKEN_SECRET,
+      //   // { expiresIn: "12h" }
+      // );
+      res.send({ result });
+    });
+    app.put("/purchase/:id", (req, res) => {
+      const id = req.params.id;
+      const total = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: total.name,
+          productName: total.productName,
+          phone: total.phone,
+          quantity: total.quantity,
+          price: total.price,
+          email: total.email,
+        },
+      };
 
-  const result = await paymentCollection.insertOne(payment);
-  const updatedPurchase = await purchaseCollection.updateOne(
-    filter,
-    updatedDoc
-  );
-  res.send(updatedPurchase);
-});
-app.get("/purchase/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: ObjectId(id) };
-  const products = await purchaseCollection.findOne(query);
-  res.send(products);
-});
-app.get("/purchase", async (req, res) => {
-  const query = {};
-  const cursor = purchaseCollection.find(query);
-  const purchases = await cursor.toArray();
-  res.send(purchases);
-});
-app.get("/purchase/:email", async (req, res) => {
-  const email = req.params.email;
-  const query = { email: email };
-  const cursor = purchaseCollection.find(query);
-  const purchases = await cursor.toArray();
-  res.send(purchases);
-});
+      const result = purchaseCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    app.patch("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+
+      const result = await paymentCollection.insertOne(payment);
+      const updatedPurchase = await purchaseCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+      res.send(updatedPurchase);
+    });
+    app.get("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const products = await purchaseCollection.findOne(query);
+      res.send(products);
+    });
+    app.get("/purchase", async (req, res) => {
+      const query = {};
+      const cursor = purchaseCollection.find(query);
+      const purchases = await cursor.toArray();
+      res.send(purchases);
+    });
+    app.get("/purchase/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const cursor = purchaseCollection.find(query);
+      const purchases = await cursor.toArray();
+      res.send(purchases);
+    });
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const orders = await orderCollenction.find(query).toArray();
+        return res.send(orders);
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    app.get("/allorders", async (req, res) => {
+      const query = {};
+      const orders = await orderCollenction.find(query).toArray();
+      console.log(orders);
+      res.send(orders);
+    });
+
+    app.patch("/paidorders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          deleverd: true,
+        },
+      };
+      const updatedOrder = await orderCollenction.updateOne(filter, updatedDoc);
+      res.send(updatedOrder);
+    });
+
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await orderCollenction.findOne(query);
+      res.send(order);
+    });
+
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+
+      const result = await paymentCollection.insertOne(payment);
+      const updatedOrder = await orderCollenction.updateOne(filter, updatedDoc);
+      res.send(updatedOrder);
+    });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await orderCollenction.deleteOne(filter);
+      res.send(result);
+    });
   } finally {
     // client.close();
   }
